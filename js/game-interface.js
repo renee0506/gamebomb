93baa9f70b858fdad3908eedca209d71e0cf4ddf
Kory Skarbek and Renee Mei <kory+renee@>
2017-03-16 16:21:15 -0700

    done

--- js/game-interface.js
+++ js/game-interface.js
@@ -1,14 +1,40 @@
 var gameSearch = require('./../js/game.js').gameSearchModule;
 var gameInfo = require('./../js/game.js').gameInfoModule;
 
+function allPlatforms(platforms){
+  $("#game-platform").empty();
+  platforms.forEach(function(platform) {
+    console.log(platform.name);
+    $("#game-platform").append(platform.name + " | ");
+  });
+}
+
+function gameDescription(info, image, releaseDate){
+  $("#game-info").text(info);
+  $("#game-date").text(releaseDate.substring(0,10));
+  $("#game-image").html("<img src='" + image + "'>");
+}
+
+function createButton(name, totalResult){
+  var number = Math.ceil(totalResult/10);
+  $("#page-buttons").empty();
+  for(var j=1; j<= number; j++){
+    $("#page-buttons").append("<button id=\"" + name + "\" class='btn page-info' type='button' value=\""+ j + "\">" + j + "</button>");
+  }
+  $(".page-info").click(function(){
+    gameSearch($(this).attr('id'), results, $(this).val(), createButton);
+  });
+}
+
 function results(data){
+  $("#search-results").empty();
   data.results.forEach(function(result) {
-    $("#search-results").append("<button class='game-info' type='button'>" + result.name + "</button></br>");
-    $(".game-info").click(function(){
-      $("#game-name").text("this is the game name");
-      $("#result").show();
-      console.log($(this).innerHTML);
-    });
+    $("#search-results").append("<br><button class='btn game-info' type='button' value=\""+ result.name + "\">" + result.name + "</button><br>");
+  });
+  $(".game-info").click(function(){
+    $("#result").show();
+    $("#game-name").text($(this).val());
+    gameInfo($(this).val(), gameDescription, allPlatforms);
   });
 }
 
@@ -16,7 +42,9 @@ $(document).ready(function(){
   $("form").submit(function(){
     event.preventDefault();
     var newName = $("#title").val();
-    gameSearch(newName, results);
+    gameSearch(newName, results, "1", createButton);
   });
-
+  $("#close").click(function(){
+    $("#result").hide();
+  })
 });
--- js/game.js
+++ js/game.js
@@ -1,24 +1,33 @@
-var gameSearch = function(name, results){
+var gameSearch = function(name, results, pageNumber, createButton){
   $.ajax({
     url: "http://api.giantbomb.com/search/",
     type: "get",
-    data: {api_key : "1deed2e185fd05e46a6e934caf89d5516d94f288", query: name, resources : "game", field_list : "name, resource_type, image", format : "json" },
-    dataType: "json"
-  })
-  .then(function(data) {
-    results(data);
-  });
-};
-
-var gameInfo = function(name, results){
-  $.ajax({
-    url: "http://api.giantbomb.com/search/",
-    type: "get",
-    data: {api_key : "1deed2e185fd05e46a6e934caf89d5516d94f288", query: name, resources : "game", field_list : "name, deck, image, original_release_date", format : "json" },
+    data: {api_key : "1deed2e185fd05e46a6e934caf89d5516d94f288", query: name, resources : "game", field_list : "name, resource_type, image", page : pageNumber, format : "json" },
     dataType: "json"
   })
   .then(function(data) {
     console.log(data);
+    results(data);
+    createButton(name, data.number_of_total_results);
+  });
+};
+
+var gameInfo = function(name, gameDescription, allPlatforms){
+  $.ajax({
+    url: "http://www.giantbomb.com/api/search/?api_key=1deed2e185fd05e46a6e934caf89d5516d94f288&format=json&query=" + name + "&resources=game",
+    // type: "get",
+    // data: {api_key : "1deed2e185fd05e46a6e934caf89d5516d94f288", query: name, resources : "game", field_list : "name, deck, image, original_release_date", format : "json" },
+    dataType: "json"
+  })
+  .then(function(data) {
+    data.results.forEach(function(result){
+      if (result.name === name)
+      {
+        gameDescription(result.deck, result.image.medium_url, result.original_release_date);
+        allPlatforms(result.platforms);
+      }
+    });
+
   });
 };
 
